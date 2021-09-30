c84db2882182076db58f92e95de3c0beea81f7d9
Dwiti Patel <49846497+dwiti9494@users.noreply.github.com>
2021-09-29 18:49:53 -0700

    Add 1-D and 2-D array code

---
+++ katas/python/1d_array.py
@@ -0,0 +1,4 @@
+# creating a 1-D array
+n = 5
+arr = [0]*n
+print(arr)
--- katas/python/2d_matrix.py
+++ katas/python/2d_matrix.py
@@ -1,9 +1,5 @@
-# # creating a 1-D array
-# n = 5
-# arr = [0]*n
-# print(arr)
-
 # create a 2-D matrix
+
 rows, cols = (5, 5)
 # arr = [[0]*cols]*rows
 arr = [[0 for i in range(cols)] for j in range(rows)]
